5956c7a090ce208efda2389e1c0c15f0d0d6f3d6
Armin <remenyiarminlaszlo@gmail.com>
2023-04-28 17:31:57 +0300

    Handover and signing classes

--- renting/src/main/java/com/renting/handover/Handover.java
+++ renting/src/main/java/com/renting/handover/Handover.java
@@ -1,13 +1,70 @@
 package com.renting.handover;
 
-// public class Handover
-// {
-//     private Integer handoverid;
-//     private LocalDate date; 
-//     private String keys;
-//     private String coldwater;
-//     private String hotwater;
-//     private String status;
-//     private String parking;
-    
-// }
+import java.time.LocalDate;
+
+public class Handover
+{
+    private Integer handoverid;
+    private LocalDate date; 
+    private String keys;
+    private String coldwater;
+    private String hotwater;
+    private String status;
+    private String signatures;
+
+    public Integer getHandoverid() {
+        return handoverid;
+    }
+    public void setHandoverid(Integer handoverid) {
+        this.handoverid = handoverid;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    public String getKeys() {
+        return keys;
+    }
+    public void setKeys(String keys) {
+        this.keys = keys;
+    }
+    public String getColdwater() {
+        return coldwater;
+    }
+    public void setColdwater(String coldwater) {
+        this.coldwater = coldwater;
+    }
+    public String getHotwater() {
+        return hotwater;
+    }
+    public void setHotwater(String hotwater) {
+        this.hotwater = hotwater;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public String getParking() {
+        return signatures;
+    }
+    public void setParking(String signatures) {
+        this.signatures = signatures;
+    }
+    public Handover(Integer handoverid, LocalDate date, String keys, String coldwater, String hotwater, String status,
+            String signatures) {
+        this.handoverid = handoverid;
+        this.date = date;
+        this.keys = keys;
+        this.coldwater = coldwater;
+        this.hotwater = hotwater;
+        this.status = status;
+        this.signatures = signatures;
+    }
+    public Handover() {
+    }
+
+}
--- renting/src/main/java/com/renting/handover/HandoverController.java
+++ renting/src/main/java/com/renting/handover/HandoverController.java
@@ -1,5 +1,58 @@
 package com.renting.handover;
 
-public class HandoverController {
-    
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HandoverController
+{
+    private List<Handover> handovers =  new ArrayList<>(Arrays.asList(
+        new Handover(1, LocalDate.of(2023, 1, 14), "yes", "yes", "yes", "Waiting", "Waiting")
+     ));
+ 
+     @GetMapping("/properties")
+     public List<Handover> getAllProperty()
+     {
+         return handovers;
+     }
+ 
+     @GetMapping("/properties/{id}")
+     public Handover getProperty(@PathVariable Integer handoverid)
+     {
+         return handovers.stream().filter(c->c.getHandoverid().equals(handoverid)).findFirst().get();
+     }
+ 
+     @PostMapping("/properties")
+     public void addProperty(@RequestBody Handover handover)
+     {
+        handovers.add(handover);    
+     }
+ 
+     @PutMapping("/properties/{id}")
+     public void updateProperty(@RequestBody Handover handover, @PathVariable Integer handoverid)
+     {
+         for (int i = 0; i < handovers.size(); i++)
+         {
+            Handover c = handovers.get(i);
+             if (c.getHandoverid().equals(handoverid)){
+                handovers.set(i, handover);
+             return;}
+         }
+     }
+ 
+     @DeleteMapping("/properties/{id}")
+     public void deleteProperty(@PathVariable Integer handoverid)
+     {
+        handovers.removeIf(c->c.getHandoverid().equals(handoverid));
+     }   
 }
